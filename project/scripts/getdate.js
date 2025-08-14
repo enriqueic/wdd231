c049b631b19b25b2ae5362622aa334a10156bb24
enriqueic <iba24001@byui.edu>
2025-08-14 01:00:38 -0700

    Site project

---
+++ project/scripts/getdate.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const yearSpan = document.getElementById("year");
+    if (yearSpan) {
+        yearSpan.textContent = new Date().getFullYear();
+    }
+    const lastMod = document.getElementById("lastModified");
+    if (lastMod) {
+        lastMod.textContent = "Last Modified: " + document.lastModified;
+    }
+});
